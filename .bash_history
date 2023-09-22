060c54433fd012f9f04ea95a8b1076ed8d589743
Schappo1612 <gustavoschappo1612@gmail.com>
2023-09-22 16:49:01 -0300

    teste

---
+++ .bash_history
@@ -0,0 +1,11 @@
+$ls
+ls
+exit
+ls
+exit
+ls
+exit
+cat /etc/fstab
+cat /var/www/html/banco.php
+ls -al /var/www/html/
+exit
---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center><h1>Página gerada no servidor
+Srv1310-9115</h1><center>";
+echo "<center><h1>Seja bem-vindo a página local do servidor WEB:<br> Instalado por: 1310-Gustavo</h1></center><br> <h1>9115-Jhenifer</h1><br> ";
+phpinfo();
+?>
+
